1ba3b09e42ad9c0b8f7b8f1c48fdc951d4e98bb1
Bruno Pachulski <yakko.acme@gmail.com>
2016-02-16 20:47:21 -0200

    Adicionando suporte para # e b

--- MusicTheory/MusicTheory/src/main/java/br/grupointegrado/musictheory/example/Main.java
+++ MusicTheory/MusicTheory/src/main/java/br/grupointegrado/musictheory/example/Main.java
@@ -11,11 +11,28 @@ public class Main {
 
     public static void main(String[] args) {
         
-        CampoHarmonicoMaior campoF = new CampoHarmonicoMaior(Nota.F);
-        System.out.println(campoF.grauNotaAcidente);
+        CampoHarmonicoMaior campoC = new CampoHarmonicoMaior(Nota.C);
+        System.out.println(campoC.grauNotaAcidente);
+        
+        CampoHarmonicoMaior campoD = new CampoHarmonicoMaior(Nota.D);
+        System.out.println(campoD.grauNotaAcidente);
         
         CampoHarmonicoMaior campoE = new CampoHarmonicoMaior(Nota.E);
         System.out.println(campoE.grauNotaAcidente);
+        
+        CampoHarmonicoMaior campoF = new CampoHarmonicoMaior(Nota.F);
+        System.out.println(campoF.grauNotaAcidente);
+        
+        CampoHarmonicoMaior campoG = new CampoHarmonicoMaior(Nota.G);
+        System.out.println(campoG.grauNotaAcidente);
+        
+        CampoHarmonicoMaior campoA = new CampoHarmonicoMaior(Nota.A);
+        System.out.println(campoA.grauNotaAcidente);
+        
+        CampoHarmonicoMaior campoB = new CampoHarmonicoMaior(Nota.B);
+        System.out.println(campoB.grauNotaAcidente);
+        
+        
         
     }
     
